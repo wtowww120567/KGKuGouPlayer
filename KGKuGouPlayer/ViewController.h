ea9af9b043fd9132a178a609d0bc2d93f2798b1c
neuedu <neuedu@wangtong.local>
2015-09-15 10:50:22 +0800

    工程的创建和MVC文件夹结构的整理，删除了ViewControllers

---
+++ KGKuGouPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KGKuGouPlayer
+//
+//  Created by neuedu on 15/9/15.
+//  Copyright (c) 2015年 wt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
